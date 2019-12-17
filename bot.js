4f916612f945a644c907965b1e0471031e2f07df
Zeval <zevalgaming@gmail.com>
2019-12-17 19:54:55 +1000

    Add comments

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
+// Iterate through the events folder and bind to all events listed in the folder (by filename)
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
@@ -18,6 +19,7 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Enmap();
 
+// Iterate through the commands folder and load the file to cache so that the commands can be invoked later
 fs.readdir("./commands/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
